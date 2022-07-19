6d8604c7f6698e973cf06f009217d5ea406e67dc
Utk2018 <47890560+Utk2018@users.noreply.github.com>
2022-07-19 11:12:21 +0530

    first commit

---
+++ static-dont-edit-approaches-to-problem-solving-in-js-forked/activity2/script.js
@@ -0,0 +1,9 @@
+/**
+ * Displays a list of elements after sorting the input array
+ * @param {Array} arr
+ */
+function generateSortedList(arr) {}
+
+generateSortedList([2, 1, 4, 3]); // Displays a list with 1, 2, 3, 4 on screen
+// generateSortedList([1, 2, 3]);
+// generateSortedList([]);
---
+++ static-dont-edit-approaches-to-problem-solving-in-js-forked/activity3/script.js
@@ -0,0 +1,13 @@
+const GITHUB_API = "https://api.github.com/users";
+
+/**
+ * Return an array of usernames fetched from the Github API
+ * https://api.github.com/users
+ */
+async function getGithubUsernames() {}
+
+/**
+ * Display the values passed as arguments as a list on the screen
+ * @param {Array} values
+ */
+function displayValuesAsList(values) {}
